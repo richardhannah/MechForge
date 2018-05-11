07df061bef2b7c0f3cdbc522ab8f00da49df4747
Richard Hannah <richard.hannah1974@gmail.com>
2018-05-11 18:38:25 +0100

    added test project

---
+++ MechForgeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MechForgeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MechForgeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64b24c03-ebd7-4763-9fa4-0b3310238905")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MechForgeTests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MechForge.Translator;
+using MechForge.Translator.Header;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MechForgeTests
+{
+    [TestClass]
+    public class FileNameTranslatorTests
+    {
+        [TestMethod]
+        public void TestTranslatorReturnsValidType()
+        {
+            string testString = "weapon_someotherstuff";
+            
+            IFileNameTranslator testSubject = new FilenameTranslator();
+            Type translatedType = testSubject.GetTypeFor(testString);
+
+            Assert.AreEqual(typeof(WeaponHeader).FullName,translatedType.FullName);
+
+        }
+
+        [TestMethod]
+        public void TestTranslatorReturnsDefaultHeader()
+        {
+            string testString = "x_someotherstuff";
+
+            IFileNameTranslator testSubject = new FilenameTranslator();
+            Type translatedType = testSubject.GetTypeFor(testString);
+
+            Assert.AreEqual(typeof(DefaultHeader).FullName, translatedType.FullName);
+
+        }
+    }
+}
